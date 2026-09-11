a8c6ff09c03b5e02dc8ad2979b363cf12aa2e589
nustanakritwithai <nustanakritwithai@gmail.com>
2026-09-11 18:37:50 +0700

    cleanup: remove retired Pirate PBR ground entry

--- pirate-fruit-offline/pocket-ground-presentation.mjs
+++
@@ -1,6 +0,0 @@
-import * as pirateFruitThree from './assets/vendor-three-RYo9rfeI.js';
-import { threeFromPirateFruitVendor } from '../asset-presentation/pirate-fruit-client-bridge.mjs?v=5';
-import { hookPirateFruitPbrGround } from '../asset-presentation/pirate-fruit-pbr-ground.mjs?v=1';
-
-const pirateFruitThreeKit = threeFromPirateFruitVendor(pirateFruitThree);
-hookPirateFruitPbrGround({ THREE: pirateFruitThreeKit });
